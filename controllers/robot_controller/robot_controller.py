8cfe97d5628c7626127772c1fc8bcde8e04e97b8
SmejoTT <adrian.trajlinek@gmail.com>
2021-01-30 18:26:58 +0000

    ROS node in controller

--- controllers/robot_controller/robot_controller.py
+++ controllers/robot_controller/robot_controller.py
@@ -3,6 +3,8 @@
 # You may need to import some classes of the controller module. Ex:
 #  from controller import Robot, Motor, DistanceSensor
 from controller import Robot
+import rospy
+from custom_msgs.msg import ControlStamped
 
 # create the Robot instance.
 robot = Robot()
@@ -10,23 +12,47 @@ robot = Robot()
 # get the time step of the current world.
 timestep = int(robot.getBasicTimeStep())
 
-# You should insert a getDevice-like function in order to get the
-# instance of a device of the robot. Something like:
-#  motor = robot.getMotor('motorname')
-#  ds = robot.getDistanceSensor('dsname')
-#  ds.enable(timestep)
+rear_right_wheel = robot.getDevice("rear_right_wheel")
+rear_left_wheel = robot.getDevice("rear_left_wheel")
+front_right_wheel = robot.getDevice("front_right_wheel")
+front_left_wheel = robot.getDevice("front_left_wheel")
+left_steer = robot.getDevice("left_steer")
+right_steer = robot.getDevice("right_steer")
+
+
+
+class Control:
+    def __init__(self):
+        rospy.init_node('control', anonymous=True)
+        self.speed = 0
+        self.acceleration = 0
+        self.steering_angle = 0
+    # Subscribers
+        self.control_sub = rospy.Subscriber('/cmd', ControlStamped, self.receive_command, queue_size=10)
+
+    # Publishers
+
+    def receive_command(self, data):	
+        self.speed = data.control.speed
+        self.acceleration = data.control.acceleration
+        self.steering_angle = data.control.steering_angle
 
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    # Read the sensors:
-    # Enter here functions to read sensor data, like:
-    #  val = ds.getValue()
-
-    # Process sensor data here.
-
-    # Enter here functions to send actuator commands, like:
-    #  motor.setPosition(10.0)
-    pass
+    control = Control()
+    rear_right_wheel.setPosition(float('+inf'))
+    rear_left_wheel.setPosition(float('+inf'))
+    front_right_wheel.setPosition(float('+inf'))
+    front_left_wheel.setPosition(float('+inf'))
+    rear_right_wheel.setVelocity(control.speed)
+    rear_left_wheel.setVelocity(control.speed)
+    front_right_wheel.setVelocity(control.speed)
+    front_left_wheel.setVelocity(control.speed)
+    left_steer.setPosition(control.steering_angle)
+    right_steer.setPosition(control.steering_angle)
+    
+    
+    
 
 # Enter here exit cleanup code.

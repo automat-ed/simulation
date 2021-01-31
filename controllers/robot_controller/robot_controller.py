9885a37296f771e7d8f23817db270f2bfb9e0bd6
SmejoTT <adrian.trajlinek@gmail.com>
2021-01-31 23:55:39 +0000

    added steering, velocity and led control

--- controllers/robot_controller/robot_controller.py
+++ controllers/robot_controller/robot_controller.py
@@ -6,6 +6,7 @@ from controller import Robot
 from controller import Motor
 import rospy
 from custom_msgs.msg import ControlStamped
+import math
 
 # create the Robot instance.
 robot = Robot()
@@ -19,6 +20,49 @@ front_right_wheel = robot.getDevice("front_right_wheel")
 front_left_wheel = robot.getDevice("front_left_wheel")
 left_steer = robot.getDevice("left_steer")
 right_steer = robot.getDevice("right_steer")
+front_right_led = robot.getDevice("front_right_led")
+front_left_led = robot.getDevice("front_left_led")
+rear_right_led = robot.getDevice("rear_right_led")
+rear_left_led = robot.getDevice("rear_left_led")
+# Distance between front and rear wheels
+WHEEL_BASE_LENGTH = 46
+CENTRE_TO_WHEEL_DISTANCE = 36
+
+def steer(angle_in_degrees):
+    # The angle in degrees should be inside <-26,26> because of the steering limit 
+    ideal_wheel_angle = math.radians(angle_in_degrees)
+    ideal_curvature_radius = WHEEL_BASE_LENGTH / math.tan(ideal_wheel_angle)
+    inside_wheel_angle = math.atan(WHEEL_BASE_LENGTH / (ideal_curvature_radius - CENTRE_TO_WHEEL_DISTANCE))
+    outside_wheel_angle = math.atan(WHEEL_BASE_LENGTH / (ideal_curvature_radius + CENTRE_TO_WHEEL_DISTANCE))
+    if ideal_wheel_angle >= 0:
+        left_steer.setPosition(inside_wheel_angle)
+        right_steer.setPosition(outside_wheel_angle)
+    else:
+        left_steer.setPosition(outside_wheel_angle)
+        right_steer.setPosition(inside_wheel_angle)
+
+def set_Velocity(speed, acceleration):
+    rear_right_wheel.setPosition(float('+inf'))
+    rear_left_wheel.setPosition(float('+inf'))
+    front_right_wheel.setPosition(float('+inf'))
+    front_left_wheel.setPosition(float('+inf'))
+    rear_right_wheel.setVelocity(control.speed)
+    rear_left_wheel.setVelocity(control.speed)
+    front_right_wheel.setVelocity(control.speed)
+    front_left_wheel.setVelocity(control.speed)
+
+def set_front_leds(value):
+    front_left_led.set(value)
+    front_right_led.set(value)
+
+def set_rear_leds(value):
+    rear_left_led.set(value)
+    rear_right_led.set(value)    
+
+
+# Turn the lights on
+set_front_leds(1)
+set_rear_leds(1)
 
 class Control:
     def __init__(self):
@@ -42,18 +86,8 @@ control = Control()
 # Main loop:
 # - perform simulation steps until Webots is stopping the controller
 while robot.step(timestep) != -1:
-    rear_right_wheel.setPosition(float('+inf'))
-    rear_left_wheel.setPosition(float('+inf'))
-    front_right_wheel.setPosition(float('+inf'))
-    front_left_wheel.setPosition(float('+inf'))
-    rear_right_wheel.setVelocity(control.speed)
-    rear_left_wheel.setVelocity(control.speed)
-    front_right_wheel.setVelocity(control.speed)
-    front_left_wheel.setVelocity(control.speed)
-    left_steer.setPosition(control.steering_angle)
-    right_steer.setPosition(control.steering_angle)
-    
-    
+    set_Velocity(control.speed, control.acceleration)
+    steer(control.steering_angle)
     
 
 # Enter here exit cleanup code.

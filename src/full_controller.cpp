990f4a1d1c9deb1926e5037e281f81186d1412fc
Vincenzo <vincenzo.incutti@gmail.com>
2021-02-16 19:37:55 +0000

    removed sensors other than imu

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -1,8 +1,4 @@
 #include "ros/ros.h"
-#include "sensors/lidar.hpp"
-#include "webots/Lidar.hpp"
-#include "sensors/gps.hpp"
-#include "webots/GPS.hpp"
 #include "sensors/inertialUnit.hpp"
 #include "webots/InertialUnit.hpp"
 #include "webots/Motor.hpp"
@@ -14,10 +10,6 @@ int main(int argc, char **argv) {
   ros::NodeHandle nh("~");
 
   // Get ROS parameters
-  std::string lidar_name;
-  nh.getParam("lidar_name", lidar_name);
-  std::string gps_name;
-  nh.getParam("gps_name", gps_name);
   std::string imu_name;
   nh.getParam("imu_name", imu_name);
   int step_size;
@@ -27,19 +19,12 @@ int main(int argc, char **argv) {
   webots::Robot *robot = new webots::Robot();
 
   // Get webot devices
-  webots::Lidar *wb_lidar = robot->getLidar("lidar");
-  webots::GPS *wb_gps = robot->getGPS("gps");
   webots::InertialUnit *wb_imu = robot->getInertialUnit("imu");
 
   // Instantiate sensor wrappers
-  AutomatED::Lidar lidar = AutomatED::Lidar(wb_lidar, &nh);
-  AutomatED::GPS gps = AutomatED::GPS(wb_gps, &nh);
   AutomatED::InertialUnit imu = AutomatED::InertialUnit(wb_imu, &nh);
 
   while (robot->step(step_size) != -1) {
-    lidar.publishPointCloud();
-    gps.publishGPSCoordinate();
-    gps.publishGPSSpeed();
     imu.publishImuQuaternion();
   }
 

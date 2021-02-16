d0faf818abd86375c03a85735bfd044f7c5005e9
Vincenzo <vincenzo.incutti@gmail.com>
2021-02-16 01:19:08 +0000

    controller publishing GPS data

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "sensors/lidar.hpp"
 #include "webots/Lidar.hpp"
+#include "sensors/gps.hpp"
+#include "webots/GPS.hpp"
 #include "webots/Motor.hpp"
 #include "webots/Robot.hpp"
 
@@ -12,6 +14,8 @@ int main(int argc, char **argv) {
   // Get ROS parameters
   std::string lidar_name;
   nh.getParam("lidar_name", lidar_name);
+  std::string gps_name;
+  nh.getParam("gps_name", gps_name);
   int step_size;
   nh.getParam("step_size", step_size);
 
@@ -20,12 +24,16 @@ int main(int argc, char **argv) {
 
   // Get webot devices
   webots::Lidar *wb_lidar = robot->getLidar("lidar");
+  webots::GPS *wb_gps = robot->getGPS("gps");
 
   // Instantiate sensor wrappers
   AutomatED::Lidar lidar = AutomatED::Lidar(wb_lidar, &nh);
+  AutomatED::GPS gps = AutomatED::GPS(wb_gps, &nh);
 
   while (robot->step(step_size) != -1) {
     lidar.publishPointCloud();
+    gps.publishGPSCoordinate();
+    gps.publishGPSSpeed();
   }
 
   // Clean up

19ac8729e88c2aaf68c2a4a54239b7770eb4dbdd
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2021-02-17 21:24:46 +0000

    Add IMU sensor to simulation (#16)

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -3,6 +3,8 @@
 #include "webots/GPS.hpp"
 #include "sensors/lidar.hpp"
 #include "webots/Lidar.hpp"
+#include "sensors/inertialUnit.hpp"
+#include "webots/InertialUnit.hpp"
 #include "webots/Motor.hpp"
 #include "webots/Robot.hpp"
 
@@ -14,6 +16,8 @@ int main(int argc, char **argv) {
   // Get ROS parameters
   std::string lidar_name;
   nh.param<std::string>("lidar/name", lidar_name, "RobotisLds01");
+  std::string imu_name;
+  nh.param<std::string>("imu/name", imu_name, "imu");
   std::string gps_name;
   nh.param<std::string>("gps/name", gps_name, "gps");
   int step_size;
@@ -25,15 +29,18 @@ int main(int argc, char **argv) {
   // Get webot devices
   webots::Lidar *wb_lidar = robot->getLidar(lidar_name);
   webots::GPS *wb_gps = robot->getGPS(gps_name);
+  webots::InertialUnit *wb_imu = robot->getInertialUnit(imu_name);
 
   // Instantiate sensor wrappers
   AutomatED::Lidar lidar = AutomatED::Lidar(wb_lidar, &nh);
   AutomatED::GPS gps = AutomatED::GPS(wb_gps, &nh);
+  AutomatED::InertialUnit imu = AutomatED::InertialUnit(wb_imu, &nh);
 
   while (robot->step(step_size) != -1) {
     lidar.publishLaserScan();
     gps.publishGPSCoordinate();
     gps.publishGPSSpeed();
+    imu.publishImuQuaternion();
   }
 
   // Clean up

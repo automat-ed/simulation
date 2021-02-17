ecb880318a030214de3010496e44f91724210cae
Vincenzo <vincenzo.incutti@gmail.com>
2021-02-17 20:47:05 +0000

    added imu

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -1,6 +1,8 @@
 #include "ros/ros.h"
 #include "sensors/lidar.hpp"
 #include "webots/Lidar.hpp"
+#include "sensors/inertialUnit.hpp"
+#include "webots/InertialUnit.hpp"
 #include "webots/Motor.hpp"
 #include "webots/Robot.hpp"
 
@@ -12,6 +14,8 @@ int main(int argc, char **argv) {
   // Get ROS parameters
   std::string lidar_name;
   nh.param<std::string>("lidar/name", lidar_name, "RobotisLds01");
+  std::string imu_name;
+  nh.param<std::string>("imu/name", imu_name, "imu");
   int step_size;
   nh.param("step_size", step_size, 32);
 
@@ -20,12 +24,15 @@ int main(int argc, char **argv) {
 
   // Get webot devices
   webots::Lidar *wb_lidar = robot->getLidar("RobotisLds01");
+  webots::InertialUnit *wb_imu = robot->getInertialUnit("imu");
 
   // Instantiate sensor wrappers
   AutomatED::Lidar lidar = AutomatED::Lidar(wb_lidar, &nh);
+  AutomatED::InertialUnit imu = AutomatED::InertialUnit(wb_imu, &nh);
 
   while (robot->step(step_size) != -1) {
     lidar.publishLaserScan();
+    imu.publishImuQuaternion();
   }
 
   // Clean up

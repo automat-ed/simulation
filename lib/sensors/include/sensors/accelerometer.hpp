e77361f7b629a0f9e8b5a29d2f07ba1dab40357f
Vincenzo Incutti <vincenzo.incutti@gmail.com>
2021-02-21 21:39:28 +0000

    Add Accelerometer to Simulation (#24)

---
+++ lib/sensors/include/sensors/accelerometer.hpp
@@ -0,0 +1,37 @@
+#include "ros/ros.h"
+#include "tf2_ros/static_transform_broadcaster.h"
+#include "webots/Accelerometer.hpp"
+#include "webots/Supervisor.hpp"
+
+namespace AutomatED {
+
+class Accelerometer {
+public:
+  Accelerometer(webots::Supervisor *webots_supervisor,
+               ros::NodeHandle *ros_handle);
+  ~Accelerometer();
+
+  void publishAccelerometer();
+
+private:
+  // Handlers
+  webots::Supervisor *wb;
+  ros::NodeHandle *nh;
+
+  // Webots devices
+  webots::Accelerometer *accelerometer;
+
+  // ROS parameters
+  std::string accelerometer_name;
+  int sampling_period;
+  std::string accelerometer_pub_topic;
+
+  // ROS publisher
+  ros::Publisher accelerometer_pub;
+
+  // Tf2
+  tf2_ros::StaticTransformBroadcaster static_broadcaster;
+  void publishTF();
+};
+
+} // namespace AutomatED
--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -2,10 +2,8 @@
 #include "sensors/gps.hpp"
 #include "sensors/inertialUnit.hpp"
 #include "sensors/lidar.hpp"
+#include "sensors/accelerometer.hpp"
 #include "steering/diffSteering.hpp"
-#include "webots/GPS.hpp"
-#include "webots/InertialUnit.hpp"
-#include "webots/Lidar.hpp"
 #include "webots/Motor.hpp"
 #include "webots/Supervisor.hpp"
 
@@ -23,10 +21,10 @@ int main(int argc, char **argv) {
 
   // Get webots motors
   webots::Motor *motors[4] = {
-    robot->getMotor("front_left_wheel"),
-    robot->getMotor("front_right_wheel"),
-    robot->getMotor("rear_left_wheel"),
-    robot->getMotor("rear_right_wheel")
+    supervisor->getMotor("front_left_wheel"),
+    supervisor->getMotor("front_right_wheel"),
+    supervisor->getMotor("rear_left_wheel"),
+    supervisor->getMotor("rear_right_wheel")
   };
   
 
@@ -34,6 +32,7 @@ int main(int argc, char **argv) {
   AutomatED::Lidar lidar = AutomatED::Lidar(supervisor, &nh);
   AutomatED::GPS gps = AutomatED::GPS(supervisor, &nh);
   AutomatED::InertialUnit imu = AutomatED::InertialUnit(supervisor, &nh);
+  AutomatED::Accelerometer accelerometer = AutomatED::Accelerometer(supervisor, &nh);
 
   // Instantiate steering
   AutomatED::DiffSteering diffSteering = AutomatED::DiffSteering(motors, &nh);
@@ -43,6 +42,7 @@ int main(int argc, char **argv) {
     gps.publishGPSCoordinate();
     gps.publishGPSSpeed();
     imu.publishImuQuaternion();
+    accelerometer.publishAccelerometer();
     ros::spinOnce();
   }
 

b5205bc8d229e7b562c68bed14763fb57e3f7b84
Vincenzo <vincenzo.incutti@gmail.com>
2021-02-21 21:56:13 +0000

    removed "Data" from function name

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -42,7 +42,7 @@ int main(int argc, char **argv) {
     gps.publishGPSCoordinate();
     gps.publishGPSSpeed();
     imu.publishImuQuaternion();
-    gyro.publishGyroImu();
+    gyro.publishGyro();
     ros::spinOnce();
   }
 

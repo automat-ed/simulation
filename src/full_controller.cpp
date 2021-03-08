a1b60eaaec8035f0695fbb5d28c91b0caa1dc802
Siliconlad <siliconlad@protonmail.com>
2021-03-08 07:24:02 +0000

    Publish wheel odom data through diffSteering

--- src/full_controller.cpp
+++ src/full_controller.cpp
@@ -55,6 +55,7 @@ int main(int argc, char **argv)
     imu.publishImuQuaternion();
     gyro.publishGyro();
     accelerometer.publishAccelerometer();
+    diffSteering.publish();
 
     if (use_keyboard_control)
     {

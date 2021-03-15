3bb675bb7b72833f8590e189541cea813c329b37
Angus Stewart <siliconlad@protonmail.com>
2021-03-15 02:27:22 +0000

    Simplify and update GPS

--- lib/sensors/src/gps.cpp
+++ lib/sensors/src/gps.cpp
@@ -67,67 +67,29 @@ void GPS::publishGPSCoordinate()
   gt.header.stamp = ros::Time::now();
   gt.header.frame_id = frame_id;
   gt.latitude = reading[0];
-  gt.longitude = reading[2];
-  gt.altitude = reading[1];
+  gt.longitude = reading[1];
+  gt.altitude = reading[2];
   gt.position_covariance_type = sensor_msgs::NavSatFix::COVARIANCE_TYPE_DIAGONAL_KNOWN;
   gt.status.service = sensor_msgs::NavSatStatus::SERVICE_GPS;
   gt_coordinate_pub.publish(gt);
-
-  // Publish data with noise
-  sensor_msgs::NavSatFix msg;
-  msg.header.stamp = ros::Time::now();
-  msg.header.frame_id = frame_id;
-  msg.latitude = reading[0] + bias + gaussianNoise();
-  msg.longitude = reading[2] + bias + gaussianNoise();
-  msg.altitude = reading[1] + bias + gaussianNoise();
-  msg.position_covariance_type =
-      sensor_msgs::NavSatFix::COVARIANCE_TYPE_DIAGONAL_KNOWN;
-  // Populate variance along the diagonal
-  msg.position_covariance[0] = std::pow(bias + noise_mean + noise_std, 2);
-  msg.position_covariance[4] = std::pow(bias + noise_mean + noise_std, 2);
-  msg.position_covariance[8] = std::pow(bias + noise_mean + noise_std, 2);
-  msg.status.service = sensor_msgs::NavSatStatus::SERVICE_GPS;
-  coordinate_pub.publish(msg);
 }
 
 void GPS::publishTF()
 {
-  // Get gps node
-  webots::Node *gps_node = wb->getFromDevice(gps);
-
-  // Get gps translation
-  webots::Field *gps_translation_field = gps_node->getField("translation");
-  const double *gps_translation = gps_translation_field->getSFVec3f();
-
-  // Get gps rotation
-  webots::Field *gps_rotation_field = gps_node->getField("rotation");
-  const double *gps_rotation = gps_rotation_field->getSFRotation();
-
   // Create transform msg
   geometry_msgs::TransformStamped msg;
   msg.header.stamp = ros::Time::now();
-  msg.header.frame_id = "odom";
+  msg.header.frame_id = "base_link";
   msg.child_frame_id = frame_id;
 
-  // Translate from Webots to ROS coordinates
-  msg.transform.translation.x = gps_translation[0];
-  msg.transform.translation.y = -1 * gps_translation[2];
-  msg.transform.translation.z = gps_translation[1];
+  msg.transform.translation.x = 0;
+  msg.transform.translation.y = 0;
+  msg.transform.translation.z = 0;
 
-  tf2::Quaternion rot;
-  rot[0] = gps_rotation[1];
-  rot[1] = gps_rotation[2];
-  rot[2] = gps_rotation[3];
-  rot[3] = gps_rotation[0];
-
-  tf2::Quaternion webots_to_ros;
-  webots_to_ros.setRPY(-1.5707, 0, 0);
-
-  tf2::Quaternion quat = webots_to_ros * rot;
-  msg.transform.rotation.x = quat.x();
-  msg.transform.rotation.y = quat.y();
-  msg.transform.rotation.z = quat.z();
-  msg.transform.rotation.w = quat.w();
+  msg.transform.rotation.x = 0;
+  msg.transform.rotation.y = 0;
+  msg.transform.rotation.z = 0;
+  msg.transform.rotation.w = 1;
 
   static_broadcaster.sendTransform(msg);
 }
